ccb3a0e470cf2d871f68615396fa6dbe59402291
Clearwater <xxx@xxx.com>
2024-12-23 18:14:24 +0100

    Time in logoutput

--- logging.php
+++ logging.php
@@ -4,21 +4,21 @@ use Codedungeon\PHPCliColors\Color;
 
 function logError($message)
 {
-    echo(Color::red() . $message . Color::reset() . "\n");
+    echo(Color::red() . "[" . date("h:i:s A") .  "] ". $message . Color::reset() . "\n");
 }
 
 function logWarn($message)
 {
-    echo(Color::yellow() . $message . Color::reset() . "\n");
+    echo(Color::yellow() . "[" . date("h:i:s A") .  "] ". $message . Color::reset() . "\n");
 }
 
 function logInfo($message)
 {
-    echo(Color::light_blue() . $message . Color::reset() . "\n");
+    echo(Color::light_blue() . "[" . date("h:i:s A") .  "] ". $message . Color::reset() . "\n");
 }
 
 
 function logMessage($message)
 {
-    echo(Color::green() . $message . Color::reset() . "\n");
+    echo(Color::green() . "[" . date("h:i:s A") .  "] ". $message . Color::reset() . "\n");
 }
